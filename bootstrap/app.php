363d5c455fbb23ffae4a23b7b8e84e92e5e5236e
radekkrab <radxxx7@mail.ru>
2024-06-24 12:43:12 +0500

    add schedule in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Queue as FacadesQueue;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,6 +24,9 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+    })
+    ->withSchedule(function(Schedule $schedule){
+        $schedule->exec('php artisan queue:work')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
