7dbd70635440271444e1de5997eb1c84385ffc8f
radekkrab <radxxx7@mail.ru>
2024-06-28 14:58:08 +0500

    final2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withSchedule(function(Schedule $schedule){
-        $schedule->command('queue:work --stop-when-empty')->everyMinute();
+        $schedule->command('queue:work')->everyMinute();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
