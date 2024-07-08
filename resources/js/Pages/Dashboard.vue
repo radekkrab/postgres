b41f2c50f09f2ba198012193646c5b32eebe1c04
radekkrab <radxxx7@mail.ru>
2024-07-08 11:38:28 +0500

    fix mob style

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -14,18 +14,18 @@ defineProps(['products', 'role']);
     <Head title="Главная" />
 
     <div class="flex">
-        <div class="left-panel w-1/6 bg-[#374050]">
+        <div class="left-panel w-1/6 bg-[#374050] hidden lg:block">
             <div class="flex">
                 <div class="logo px-6 py-8  bg-white rounded-br-[40px]"><img class="w-24" src="/img/logo.png" alt="logo"></div>
                 <p class="text-white font-roboto text-lg px-5 py-3 leading-5">Enterprise<br>Resourse<br>Planning</p>
             </div>
             <p class="text-[#FFFFFF99] font-roboto text-lg mt-6 ml-16 leading-5"> <a href="#">Продукты</a></p>
         </div>
-        <div class="right-panel w-5/6">
+        <div class="right-panel lg:w-5/6">
             <AuthenticatedLayout>
-            <div class="flex justify-between bg-[#F2F6FA] h-screen">
+            <div class="flex flex-col lg:flex-row lg:justify-between bg-[#F2F6FA] h-screen">
                 <div class="table w-3/4"><ViewProducts /></div>
-                <div class="button pr-4"><AddProduct /></div>
+                <div class="button pr-4 self-end lg:self-auto"><AddProduct /></div>
             </div>
             </AuthenticatedLayout>
         </div>
